bb80c9f0e2a5e4ab4e7ea5ada820064c83d7c061
VNagorniy <vnagorniyrnd@gmail.com>
2023-05-23 23:34:22 +0300

    реализация header(hamburger-menu, добавление react-icons,стилизация)

---
+++ src/components/layout/header/Header.jsx
@@ -0,0 +1,19 @@
+import { useAuth } from '../../../hooks/useAuth'
+import Hamburger from '../hamburger/Hamburger'
+import styles from './Header.module.scss'
+import { FiArrowLeft } from 'react-icons/fi'
+
+const Header = ({ backLink }) => {
+	const { isAuth } = useAuth()
+	return (
+		<header className={styles.header}>
+			<button onClick={() => {}}>
+				<FiArrowLeft />
+			</button>
+			{/* {User profile} */}
+			<Hamburger />
+		</header>
+	)
+}
+
+export default Header
