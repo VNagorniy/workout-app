4f13474538fe0d1c844aeedfd7dc91e9bc350b0a
VNagorniy <vnagorniyrnd@gmail.com>
2023-05-25 23:22:01 +0300

    добавление react-router-dom, сортировка импортов, доработка компонента Layout

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,14 +1,17 @@
+import { IoMdArrowBack } from 'react-icons/io'
+
 import { useAuth } from '../../../hooks/useAuth'
+
 import Hamburger from '../hamburger/Hamburger'
+
 import styles from './Header.module.scss'
-import { FiArrowLeft } from 'react-icons/fi'
 
 const Header = ({ backLink }) => {
 	const { isAuth } = useAuth()
 	return (
 		<header className={styles.header}>
 			<button onClick={() => {}}>
-				<FiArrowLeft />
+				<IoMdArrowBack fill='#fff' fontSize={29} />
 			</button>
 			{/* {User profile} */}
 			<Hamburger />
