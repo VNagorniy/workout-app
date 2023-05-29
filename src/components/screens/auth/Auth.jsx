9047dbbd356919eb13f9a9274e369088171a2fef
VNagorniy <vnagorniyrnd@gmail.com>
2023-05-29 23:53:05 +0300

    установка axios и реализация запроса с токеном

--- src/components/screens/auth/Auth.jsx
+++ src/components/screens/auth/Auth.jsx
@@ -1,3 +1,4 @@
+import { useMutation } from '@tanstack/react-query'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
@@ -5,32 +6,42 @@ import Loader from '../../ui/Loader'
 import Button from '../../ui/button/Button'
 import Field from '../../ui/field/Field'
 
+import AuthService from '../../../services/auth.service'
 import Layout from '../../layout/Layout'
 
 import styles from './Auth.module.scss'
 
-const isLoading = false
-const isLoadingAuth = false
-
 const Auth = () => {
-	const [type, setType] = useState('auth')
+	const [type, setType] = useState('login')
 	const {
 		register,
 		handleSubmit,
-		formState: { errors }
+		formState: { errors },
+		reset
 	} = useForm({
 		mode: 'onChange'
 	})
 
+	const { mutate, isLoading } = useMutation(
+		['auth'],
+		({ email, password }) => AuthService.main(email, password, type),
+		{
+			onSuccess: data => {
+				alert('Success')
+				reset()
+			}
+		}
+	)
+
 	const onSubmit = data => {
-		console.log(data)
+		mutate(data)
 	}
 
 	return (
 		<>
 			<Layout heading='Sign in' bgImage='./images/auth-bg.png' />
 			<div className='wrapper-inner-page'>
-				{(isLoading || isLoadingAuth) && <Loader />}
+				{isLoading && <Loader />}
 				<form onSubmit={handleSubmit(onSubmit)}>
 					<Field
 						error={errors?.email?.message}
@@ -53,8 +64,8 @@ const Auth = () => {
 						placeholder='Enter password'
 					/>
 					<div className={styles.wrapperButtons}>
-						<Button clickHandler={() => setType('auth')}>Sign in</Button>
-						<Button clickHandler={() => setType('reg')}>Sign up</Button>
+						<Button clickHandler={() => setType('login')}>Sign in</Button>
+						<Button clickHandler={() => setType('register')}>Sign up</Button>
 					</div>
 				</form>
 			</div>
